331e2995f2e54bcd820e431f5ee55344cd725f00
Luke Pafford <lukepafford@gmail.com>
2018-01-01 18:21:31 -0800

    Removed unused file config.py

--- service_check/config.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-import sys, os
-PACKAGE_ROOT = os.path.abspath(os.path.dirname(__name__))
-sys.path.append(PACKAGE_ROOT)
-
-# Your current problem is that you cannot reference the correct package location
-# once you have built the package
