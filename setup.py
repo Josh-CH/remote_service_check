41c0e5dadfc312db928e3ad42da1c68d0bf37118
Luke Pafford <lukepafford@gmail.com>
2018-01-01 18:18:15 -0800

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='service_check',
 	version='1.0.1',
@@ -6,8 +6,21 @@ setup(name='service_check',
 	author='Luke Pafford',
 	author_email='lukepafford@gmail.com',
 	license='MIT',
-	packages=['service_check'],
+	packages=find_packages(exclude=['test']),
 	zip_safe=False,
+	install_requires=['colorama', 'ansible'],
+	classifiers=[
+			'Development Status :: 5 - Production/Stable',
+			'Intended Audience :: System Administrators',
+			'Environment :: Console',
+			'Operating System :: POSIX :: Linux',
+			'Programming Language :: Python :: 3',
+			'Topic :: System :: Monitoring',
+			],
+	keywords='ansible sqlite3 monitoring',
+	python_requires='>=3',
+	package_data={'': ['scripts/*', 'db'] },
+	include_package_data=True,
 	entry_points={
 		'console_scripts': [
 				'service_check = service_check.__main__:main'
